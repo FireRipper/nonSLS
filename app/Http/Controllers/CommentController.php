e149ca9ae743196cba0641c6844258739344a311
Illia <shabelnyk.ilya@gmail.com>
2019-05-20 12:04:06 +0300

    Update CommentController, work comment on page.

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Comment;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use App\Http\Requests\CommentRequest;
 
 class CommentController extends Controller
@@ -12,21 +11,10 @@ class CommentController extends Controller
     public function store(CommentRequest $request)
     {
 
-        $first_name = $request->input('name');
-        $text_comment = $request->input('comment');
-
-        $user = Auth::user();
-        $add = new Comment;
-
-        if($first_name == DB::table('users')->value('name'))
-        {
-            if ($user) {
-                $user_id = $data['user_id'] = $user->id;
-                $add->fill($text_comment, $user_id);
-            }
-        }
-
-        $add->save();
+        $commentModel = new Comment();
+        $commentModel->comment = $request->comment;
+        $commentModel->user_id = auth()->id();
+        $isSaved = $commentModel->save();
 
         return redirect()->route('main');
     }
