9fe5296879c8a29e4a12b332ae30303ae7c544b4
Illia <shabelnyk.ilya@gmail.com>
2019-06-11 01:23:40 +0300

    Added new content on admin page

--- resources/views/admin/adminpanel.blade.php
+++ resources/views/admin/adminpanel.blade.php
@@ -57,44 +57,66 @@
         </div>
       </div>
       <div class="col-lg-6 mb-4 mt-sm-3 mb-lg-0">
-        <div class="card">
-          <div class="card-header">
-            <h2 class="h6 text-uppercase mb-0">Входящие</h2>
+        <div class="card content-admin-card">
+          <div class="card-header content-admin-card__header bg-card-header__blue">
+            <h2 class="content-admin-card__header__h2 text-uppercase mb-0">Входящие</h2>
           </div>
-          <div class="card-body">
-            <p class="text-gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-6 mb-4 mt-sm-3 mb-lg-0">
-        <div class="card">
-          <div class="card-header">
-            <h2 class="h6 text-uppercase mb-0">Current server uptime</h2>
-          </div>
-          <div class="card-body">
-            <p class="text-gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-
+          <div class="card-body content-admin-card__body">
+            <p class="content-admin-card__body__text">В данном разделе:</p>
+            <ul class="list-group list-group-flush content-admin-card__body__list">
+              <li class="list-group-item">1. Заявки всех пользователей находяться в табдице</li>
+              <li class="list-group-item">2. Возможно <strong> просмотреть заявку</strong>, кликнуть на <b>"Перейти"</b>  в конце строки</li>
+              <li class="list-group-item">3. Возможно <strong> отправить заявку в раздел</strong> "Прочитаные"</li>
+              <li class="list-group-item">4. Возможно <strong>копировать e-mail</strong> пользователя просто нажать на строку e-mail!</li>
+            </ul>
           </div>
         </div>
       </div>
       <div class="col-lg-6 mb-4 mt-sm-3 mb-lg-0">
-        <div class="card">
-          <div class="card-header">
-            <h2 class="h6 text-uppercase mb-0">Current server uptime</h2>
+        <div class="card content-admin-card">
+          <div class="card-header content-admin-card__header bg-card-header__green">
+            <h2 class="content-admin-card__header__h2 text-uppercase mb-0">Прочитаные</h2>
           </div>
-          <div class="card-body">
-            <p class="text-gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-
+          <div class="card-body content-admin-card__body">
+            <p class="content-admin-card__body__text">В данном разделе:</p>
+            <ul class="list-group list-group-flush content-admin-card__body__list">
+              <li class="list-group-item">1. Заявки всех пользователей находяться в табдице</li>
+              <li class="list-group-item">2. Возможно <strong> просмотреть заявку</strong>, кликнуть на <b>"Перейти"</b>  в конце строки</li>
+              <li class="list-group-item">3. Возможно <strong> отправить заявку в раздел</strong> "Прочитаные"</li>
+              <li class="list-group-item">4. Возможно <strong>копировать e-mail</strong> пользователя просто нажать на строку e-mail!</li>
+            </ul>
           </div>
         </div>
       </div>
       <div class="col-lg-6 mb-4 mt-sm-3 mb-lg-0">
-        <div class="card">
-          <div class="card-header">
-            <h2 class="h6 text-uppercase mb-0">Current server uptime</h2>
+        <div class="card content-admin-card">
+          <div class="card-header content-admin-card__header bg-card-header__indigo">
+            <h2 class="content-admin-card__header__h2 text-uppercase mb-0">Техническое задание</h2>
           </div>
-          <div class="card-body">
-            <p class="text-gray">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <div class="card-body content-admin-card__body">
+            <p class="content-admin-card__body__text">В данном разделе:</p>
+            <ul class="list-group list-group-flush content-admin-card__body__list">
+              <li class="list-group-item">1. Расположена <b>форма</b> на странице для ввода таких данных: <b>"E-mail пользователя"</b> и <b>"Техническое задание для пользователя"</b></li>
+              <li class="list-group-item">2. Вводим <b>"E-mail пользователя"</b> для отправки технического задания <b>конкретному пользователю</b></li>
+              <li class="list-group-item">3. Вводим <b>"Техническое задание"</b> которое было <b>сформировано при разговоре</b> клиента со специалистом</li>
+              <li class="list-group-item">4. <b>Отпрвляються данные</b> для пользователя <b>после заполнения всех полей</b></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 mb-4 mt-sm-3 mb-lg-0">
+        <div class="card content-admin-card">
+          <div class="card-header content-admin-card__header bg-card-header__red">
+            <h2 class="content-admin-card__header__h2 text-uppercase mb-0">Пользователи</h2>
+          </div>
+          <div class="card-body content-admin-card__body">
+            <p class="content-admin-card__body__text">В данном разделе:</p>
+            <ul class="list-group list-group-flush content-admin-card__body__list">
+              <li class="list-group-item">1. Все <b>зарегистрированные</b> пользователи</li>
+              <li class="list-group-item">2. Возможно <strong> просмотреть заявку</strong>, кликнуть на <b>"Перейти"</b>  в конце строки</li>
+              <li class="list-group-item">3. Возможно <strong> отправить заявку в раздел</strong> "Прочитаные"</li>
+              <li class="list-group-item">4. Возможно <strong>копировать e-mail</strong> пользователя просто нажать на строку e-mail!</li>
+            </ul>
           </div>
         </div>
       </div>
